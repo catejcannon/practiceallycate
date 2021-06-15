5d54712c5ac15cf2fa8407b5d68f777b71b622ff
Cate Cannon <catejewel@icloud.com>
2021-06-15 14:18:07 -0400

    Initial Commit

---
+++ practiceallycate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practiceallycate
+//
+//  Created by Cate J. Cannon on 6/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
